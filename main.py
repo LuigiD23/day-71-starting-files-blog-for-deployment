c5c95ec4192927b427e2deba01be2d69019073e5
luigidiaz2312@gmail.comgit config --global user.name luigidiaz2312@gmail.comgit config --global user.name luigidiaz2312@gmail.com <luigidiaz2312@gmail.com>
2023-09-27 16:38:30 -0300

    Use env variables and reate Procfile.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import ForeignKey
 # Import your forms from the forms.py
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+import os
 
 
 
@@ -28,14 +29,14 @@ This will install the packages from the requirements.txt for this project.
 '''
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
 # TODO: Configure Flask-Login
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Luigi/OneDrive/Desktop/PycharmProjects/day-69-starting-files-blog-with-users/instance/posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
@@ -265,4 +266,4 @@ def contact():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=False)
